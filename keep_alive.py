25269577c2d3e075d0a2eee29ae2baa5835b401e
Bloxxness <113144430+Bloxxness@users.noreply.github.com>
2025-05-01 17:11:17 -0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -3,15 +3,12 @@ from threading import Thread
 
 app = Flask('')
 
-
 @app.route('/')
 def home():
-    return "Bot is alive!"
-
+    return "I'm alive"
 
 def run():
     app.run(host='0.0.0.0', port=8000)
-
 
 def keep_alive():
     t = Thread(target=run)
